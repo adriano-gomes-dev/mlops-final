0eae34df5d6220bca20bef60890a17ace99e3f47
Adriano Gebert Gomes <124310443+adriano-gomes-dev@users.noreply.github.com>
2025-04-20 15:36:28 -0300

    adicionando bibliotecas do projeto: deployment, logs, trains

---
+++ deployment.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import mlflow
+
+from mlflow.tracking import MlflowClient
+
+def promote_model_to_production_based_on_mse():
+    client = MlflowClient()
+    list_metrics_version = []
+    # List all versions of a model
+    for mv in client.search_model_versions("name='incc_model'"):
+        print(f"Version: {mv.version}, Stage: {mv.current_stage}")
+
+        # Step 1: Get run_id from model version
+        model_version_info = client.get_model_version(name=mv.name, version=mv.version)
+        run_id = model_version_info.run_id
+
+        # Step 2: Get metrics
+        run = client.get_run(run_id)
+        metrics = run.data.metrics['mse']
+
+        list_metrics_version.append({'version': mv.version, 'mse': metrics})
+        
+    df_metrics_version = pd.DataFrame(list_metrics_version)
+    min_mse = df_metrics_version['mse'].min()
+    min_mse_version = df_metrics_version[df_metrics_version['mse'] == min_mse]['version'].values[0]
+    print(f"Versão com o menor MSE: {min_mse_version}")
+
+    client.transition_model_version_stage(
+        name=mv.name,
+        version=min_mse_version,
+        stage="Production"
+    )
+
+def get_prediction_from_production_model(value_to_predict):
+    # Set the tracking URI to the local MLflow server
+    # precisa chamar aqui novamente por causa do fastapi
+    mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
+    client = MlflowClient()
+
+    model_production = None
+
+    for mv in client.search_model_versions("name='incc_model'"):
+        print(f"Version: {mv.version}, Stage: {mv.current_stage}")
+
+        # Step 1: Get run_id from model version
+        model_version_info = client.get_model_version(name=mv.name, version=mv.version)
+        run_id = model_version_info.run_id
+
+        if mv.current_stage == "Production":
+            model_production = mlflow.pyfunc.load_model(model_uri=f"models:/{mv.name}/{mv.version}")
+            break
+    
+    data_to_predict = pd.DataFrame({'Data': [value_to_predict]})
+    predictions = model_production.predict(data_to_predict)
+    return str(predictions[0][0])
---
+++ trains.py
@@ -0,0 +1,41 @@
+import mlflow
+
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVR
+
+from logs import log_metrics
+
+def experiment_linear_regression(X_train, X_test, y_train, y_test):
+
+    with mlflow.start_run() as run:
+        mlflow.log_param("model_type", "LinearRegression")
+
+        model = LinearRegression()
+        model.fit(X_train, y_train)
+
+        y_pred = model.predict(X_test)
+
+        log_metrics(y_test, y_pred)
+
+        print(f"Run ID: {run.info.run_id}")
+
+        mlflow.sklearn.log_model(model, "linear_regression_model", registered_model_name="incc_model")
+        print(f"Modelo Linear Regression registrado no MLflow! Run ID: {run.info.run_id}")
+
+def experiment_svr(X_train, X_test, y_train, y_test):
+    
+    with mlflow.start_run() as run:
+        mlflow.log_param("model_type", "Linear SVR")
+
+        model = SVR(kernel='linear')
+        model.fit(X_train, y_train)
+
+        y_pred = model.predict(X_test)
+        
+        log_metrics(y_test, y_pred)
+
+        print(f"Run ID: {run.info.run_id}")
+
+        mlflow.sklearn.log_model(model, "linear_SVR_model", registered_model_name="incc_model")
+        print(f"Modelo Linear SRV registrado no MLflow! Run ID: {run.info.run_id}")
