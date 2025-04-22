a7d44f6151d102ed24896500358786356591c133
Adriano Gebert Gomes <124310443+adriano-gomes-dev@users.noreply.github.com>
2025-04-21 21:12:27 -0300

    ajuste usar módulo core.deployment.py

--- prediction_api.py
+++ prediction_api.py
@@ -42,22 +42,3 @@ async def read_root():
 async def predict(date: str):
     return {"date to predict": get_prediction_from_production_model(date)}
 
-def get_prediction_from_production_model(value_to_predict):
-    # Set the tracking URI to the local MLflow server
-    # precisa chamar aqui novamente por causa do fastapi
-    mlflow.set_tracking_uri("http://127.0.0.1:5000")
-
-    client = MlflowClient()
-
-    model_production = None
-
-    for mv in client.search_model_versions("name='incc_model'"):
-        print(f"Version: {mv.version}, Stage: {mv.current_stage}")
-        
-        if mv.current_stage == "Production":
-            model_production = mlflow.pyfunc.load_model(model_uri=f"models:/{mv.name}/{mv.version}")
-            break
-    
-    data_to_predict = pd.DataFrame({'Data': [value_to_predict]})
-    predictions = model_production.predict(data_to_predict)
-    return str(predictions[0][0])
